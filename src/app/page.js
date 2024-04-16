7dc54152671ea74fd98763ae6b6da32b3eec6813
Mercenary <oaishazher@gmail.com>
2024-04-16 20:18:12 +0530

    Responsive

--- src/app/page.js
+++ src/app/page.js
@@ -43,7 +43,7 @@ export default function Home() {
 
     return (
         <main className="flex min-h-screen flex-col items-center p-24">
-            <h1 className="text-7xl font-bold text-center" style={{color: 'var(--fg)'}}>
+            <h1 className="text-6xl font-bold text-center" style={{color: 'var(--fg)'}}>
                 URL SHORTENER
             </h1>
             <p>Shorten URL with ease</p>
